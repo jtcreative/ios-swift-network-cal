ed811a34e0089e29ff47a9d64f36291e4ca215b8
James Timberlake <jamestimberlakejr@gmail.com>
2014-07-29 23:53:40 -0400

    network test

--- Network Test/AppDelegate.swift
+++ Network Test/AppDelegate.swift
@@ -9,38 +9,140 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
+
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate, NSURLConnectionDataDelegate {
+    
+    
+    
+    var connData : NSMutableData!
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+    
+    
+    
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        
         // Override point for customization after application launch.
+        
+        
+        
+        var url : NSURL = NSURL.URLWithString("your-rest-url-here");
+        
+        
+        
+        var request : NSURLRequest = NSURLRequest(URL: url);
+        
+        
+        
+        var connection : NSURLConnection = NSURLConnection(request: request, delegate: self);
+        
+        
+        
+        connection.start();
+        
+        
+        
         return true
+        
     }
-
-    func applicationWillResignActive(application: UIApplication!) {
+    
+    
+    
+    func connection(__connection: NSURLConnection!,
+        
+        didReceiveData data: NSData!){
+            
+            
+            
+            if(connData == nil)
+                
+            {
+                
+                connData = NSMutableData(data: data);
+                
+            }else{
+                
+                connData.appendData(data);
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    }
+    
+    
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!){
+        
+        var responseStr : NSString = NSString(data: connData, encoding: NSUTF8StringEncoding);
+        
+        
+        
+        var json : NSArray  = NSJSONSerialization.JSONObjectWithData(connData, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray ;
+        
+        
+        
+        var object1 = json[0];
+        
+    }
+    
+    
+    
+    func applicationWillResignActive(application: UIApplication) {
+        
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
     }
-
-    func applicationDidEnterBackground(application: UIApplication!) {
+    
+    
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
     }
-
-    func applicationWillEnterForeground(application: UIApplication!) {
+    
+    
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
     }
-
-    func applicationDidBecomeActive(application: UIApplication!) {
+    
+    
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
-
-    func applicationWillTerminate(application: UIApplication!) {
+    
+    
+    
+    func applicationWillTerminate(application: UIApplication) {
+        
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
-
-
+    
+    
+    
+    
+    
 }
 
