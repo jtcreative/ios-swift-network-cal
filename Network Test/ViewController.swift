08fe471d84892c1c9bbe46a6104c92e01dc3142d
James Timberlake <jamestimberlakejr@gmail.com>
2014-07-29 23:51:56 -0400

    Initial Commit

---
+++ Network Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Network Test
+//
+//  Created by James Timberlake on 7/29/14.
+//  Copyright (c) 2014 MB_Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
